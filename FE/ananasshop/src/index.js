8255177947758629c67f53867db1a35d6cc1685f
HUng <thanhhung6924@gmail.com>
2024-11-05 16:54:00 +0700

    done b2

--- FE/ananasshop/src/index.js
+++ FE/ananasshop/src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/users/Home/Home";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Test</h1>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Home />);
---
+++ FE/ananasshop/src/pages/users/Home/Home.js
@@ -0,0 +1,12 @@
+import { memo } from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <h1>Body</h1>
+      <h1>Footer</h1>
+    </>
+  );
+}
+export default memo(Home);
